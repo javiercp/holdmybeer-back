a0789b898429d95d6dc9f43aece5be519faf6c8a
Javier Campos Pequeño <javiercp@gmail.com>
2018-06-04 22:35:44 +0200

    correct sintax

--- chigre/models.py
+++ chigre/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Brewery(models.Model)
+class Brewery(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, default='')
