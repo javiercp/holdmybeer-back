8dc62bcbe90e557085391128a74dceb3a9a65e8c
Javier Campos Pequeño <javiercp@gmail.com>
2018-06-05 21:21:49 +0200

    views refactor

--- chigre/urls.py
+++ chigre/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from chigre import views
 
 urlpatterns = [
     url(r'^breweries/$', views.brewery_list),
     url(r'^breweries/(?P<pk>[0-9]+)/$', views.brewery_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- chigre/views.py
+++ chigre/views.py
@@ -2,53 +2,50 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from chigre.models import Brewery
 from chigre.serializers import BrewerySerializer
 
-@csrf_exempt
-def brewery_list(request):
+@api_view(['GET', 'POST'])
+def brewery_list(request, format=None):
     """
     List all breweries, or create a new brewery.
     """
     if request.method == 'GET':
         breweries = Brewery.objects.all()
         serializer = BrewerySerializer(breweries, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BrewerySerializer(data=data)
+        return Response(serializer.data)
+        
+    elif request.method == 'POST':      
+        serializer = BrewerySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def brewery_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def brewery_detail(request, pk, format=None):
     """
     Retrieve, update or delete a brewery.
     """
     try:
         brewery = Brewery.objects.get(pk=pk)
     except Brewery.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = BrewerySerializer(brewery)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BrewerySerializer(brewery, data=data)
+        serializer = BrewerySerializer(brewery, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         brewery.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
