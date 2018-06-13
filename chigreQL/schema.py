4ed930fb8965023341ed71ead9a6cb1239d14686
Javier Campos <javiercp@Javiers-MacBook-Pro.local>
2018-06-13 19:41:45 +0200

    full model in schema

--- chigreQL/schema.py
+++ chigreQL/schema.py
@@ -9,16 +9,80 @@ from chigre.models import Brewery, BeerType, KegType, Beer, Keg, TapType, Tap
 class BreweryNode(DjangoObjectType):
     class Meta:
         model = Brewery
+        exclude_fields = ('created')
         filter_fields = {
-            'name': ['exact','istartswith', 'icontains'],
+            'name': ['exact', 'istartswith', 'icontains'],
             'description': ['istartswith', 'icontains'],
             'country': ['exact'],
+            }
+        interfaces = (Node, )
+
+class BeerTypeNode(DjangoObjectType):
+    class Meta:
+        model = BeerType
+        exclude_fields = ('created')
+        filter_fields = {
+            'name': ['exact', 'istartswith', 'icontains'],
+            'description': ['istartswith', 'icontains'],
+            }
+        interfaces = (Node, )
+
+class KegTypeNode(DjangoObjectType):
+    class Meta:
+        model = KegType
+        filter_fields = {
+            'name': ['exact', 'istartswith', 'icontains'],
+            'size': ['exact'],
+            }
+        interfaces = (Node, )
+
+class TapTypeNode(DjangoObjectType):
+    class Meta:
+        model = TapType
+        filter_fields = {
+            'name': ['exact', 'istartswith', 'icontains'],
+            }
+        interfaces = (Node, )
+
+class BeerNode(DjangoObjectType):
+    class Meta:
+        model = Beer
+        filter_fields = {
+            'name': ['exact', 'istartswith', 'icontains'],
+            'description': ['istartswith', 'icontains'],
+            }
+        interfaces = (Node, )
+
+class KegNode(DjangoObjectType):
+    class Meta:
+        model = Keg
+        filter_fields = {
+            }
+        interfaces = (Node, )
+
+class TapNode(DjangoObjectType):
+    class Meta:
+        model = Tap
+        filter_fields = {
+            'number': ['exact'],
             }
         interfaces = (Node, )
 
 class Query(ObjectType):    
     breweries = Node.Field(BreweryNode)
     all_breweries = DjangoFilterConnectionField(BreweryNode)
+    beertypes = Node.Field(BeerTypeNode)
+    all_beertypes = DjangoFilterConnectionField(BeerTypeNode)
+    kegtypes = Node.Field(KegTypeNode)
+    all_kegtypes = DjangoFilterConnectionField(KegTypeNode)
+    taptypes = Node.Field(TapTypeNode)
+    all_taptypes = DjangoFilterConnectionField(TapTypeNode)
+    beers = Node.Field(BeerNode)
+    all_beers = DjangoFilterConnectionField(BeerNode)
+    kegs = Node.Field(KegNode)
+    all_kegs = DjangoFilterConnectionField(KegNode)
+    taps = Node.Field(TapNode)
+    all_taps = DjangoFilterConnectionField(TapNode)
         
 schema = graphene.Schema(
     query=Query,
