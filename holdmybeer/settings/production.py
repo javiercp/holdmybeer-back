0b6e61483b2fe16e8fa07d2928a9132f3b0a00b1
Javier Campos <javiercp@Javiers-MacBook-Pro.local>
2018-07-16 20:47:25 +0200

    settings typo

--- holdmybeer/settings/production.py
+++ holdmybeer/settings/production.py
@@ -78,7 +78,12 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Remove browsable api in prod ENABLE_BROWSABLE_API
 if os.environ.get('ENABLE_BROWSABLE_API') != 'TRUE':
-    REST_FRAMEWORK += {
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.BasicAuthentication',
+        ),
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
         )
