f34777a7acc6e35fb6bbcb8c5a3d3347d24095d2
Javier Campos <javiercp@Javiers-MacBook-Pro.local>
2018-07-16 20:39:29 +0200

    lat, long precission

--- chigre/models/pub.py
+++ chigre/models/pub.py
@@ -8,8 +8,8 @@ class Pub(SingletonModel):
     motto = models.TextField(blank=True, default='')
     description = models.TextField(blank=True, default='')
     address = models.TextField(blank=True, default='')
-    lat = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    lng = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    lat = models.DecimalField(max_digits=20, decimal_places=16, null=True, blank=True)
+    lng = models.DecimalField(max_digits=20, decimal_places=16, null=True, blank=True)
     telephone = models.CharField(max_length=50, blank=True, default='')
     logo = models.CharField(max_length=100, blank=True, default='')
     
